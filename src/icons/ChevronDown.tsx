d98a3f5402da89f88194a7e0348a5dd4ef90c04a
Serkan Bircan <serkanbircan21@yandex.com>
2024-02-12 00:40:41 +0300

    Add icons

---
+++ src/icons/ChevronDown.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from "react";
+
+const ChevronDownIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-6"
+    fill="none"
+    viewBox="0 0 86 43"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M46.26 41.196 82.778 9.791C86.294 6.767 84.156 1 79.518 1H6.482C1.844 1-.294 6.767 3.222 9.79L39.74 41.197a5 5 0 0 0 6.52 0Z"
+    />
+  </svg>
+);
+export default ChevronDownIcon;
